d90c9b8e74385a6ee47211c69b54dd610af2f4fe
bruno santos <devbrunosantos@gmail.com>
2024-03-10 20:56:09 -0300

    feat: Contact

---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function Input() {
+  const [testeInput, setTesteInput] = useState('');
+  const [numberSize, setNumberSize] = useState(0);
+
+  useEffect(() => {
+    setNumberSize(testeInput.length + 1);
+  }, [testeInput]);
+
+  // text-pink-400
+
+  return (
+    <input
+      type='text'
+      className='inline-flex text-lime-400 bg-transparent border-none outline-none'
+      onChange={(e) => setTesteInput(e.target.value)}
+      style={{
+        width: numberSize === 1 ? 16 : numberSize + 'rem',
+      }}
+      required
+    />
+  );
+}
