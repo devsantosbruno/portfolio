c0d3e2be4b641d90a9ff17d947ef3573d481a22f
bruno santos <devbrunosantos@gmail.com>
2024-03-19 20:19:42 -0300

    chore: mock

---
+++ src/mocks/courses.ts
@@ -0,0 +1,36 @@
+import type { CardProps } from "@/components/Courses/Card";
+
+export const courses: CardProps[] = [
+	{
+		company: "Rocketseat",
+		image: "",
+		link: "",
+		logo: "images/rocketseat.svg",
+		techs: ["Back-End", "Clean Code", "Front-End", "Soft Skills"],
+		year: "2021",
+	},
+	{
+		company: "JStack",
+		image: "",
+		link: "",
+		logo: "images/jstack.png",
+		techs: ["Back-End", "Front-End"],
+		year: "2024",
+	},
+	{
+		company: "Rocketseat",
+		image: "",
+		link: "",
+		logo: "images/rocketseat.svg",
+		techs: ["", ""],
+		year: "",
+	},
+	{
+		company: "Rocketseat",
+		image: "",
+		link: "",
+		logo: "images/rocketseat.svg",
+		techs: ["", ""],
+		year: "",
+	},
+];
