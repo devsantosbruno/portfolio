1daaf9a2c9f2ccf1483fb73d76c5e5c4cc51e872
bruno santos <devbrunosantos@gmail.com>
2024-03-20 18:49:26 -0300

    feat: layout with Header and Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
 import { SmoothScroll } from '@/components/ui/smooth-scroll';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -18,9 +20,13 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <Header />
+
         <SmoothScroll>
           <main className='min-h-screen w-screen bg-black'>{children}</main>
         </SmoothScroll>
+
+        <Footer />
       </body>
     </html>
   );
