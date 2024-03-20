2339e5754fbe477fc0ce8f570f9bc835ca651d90
bruno santos <devbrunosantos@gmail.com>
2024-03-19 21:21:38 -0300

    feat: submit button

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -37,6 +37,12 @@ export function Contact() {
         </span>{' '}
         Entraremos em contato para um agendamento, muito obrigado.
       </h2>
+
+      <div className='mt-5 flex justify-end'>
+        <button className='inline-flex animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#a3e635,55%,#000103)] bg-[length:200%_100%] disabled:bg-none disabled:opacity-50 px-10 py-3 font-medium text-slate-400 transition-colors'>
+          submit
+        </button>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,11 +60,20 @@ const config = {
 					from: { height: "var(--radix-accordion-content-height)" },
 					to: { height: "0" },
 				},
+				shimmer: {
+					from: {
+						backgroundPosition: "0 0",
+					},
+					to: {
+						backgroundPosition: "-200% 0",
+					},
+				},
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
 				"menu-cubic": "all 0.5s cubic-bezier(0.76, 0, 0.24, 1)",
+				shimmer: "shimmer 2s linear infinite",
 			},
 		},
 	},
