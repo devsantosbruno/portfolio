5d61ad5b9793716c3535952864f55781b203d415
bruno santos <devbrunosantos@gmail.com>
2024-03-20 18:39:50 -0300

    style: Courses animation for Footer

--- src/components/Courses/index.tsx
+++ src/components/Courses/index.tsx
@@ -2,14 +2,20 @@
 
 import { courses } from '@/mocks/courses';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 import { Card } from './Card';
 
 export function Courses() {
-  const { scrollYProgress } = useScroll();
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'end start'],
+  });
   const customTransform = useTransform(scrollYProgress, [0, 1], [-3999, 999]);
+  const height = useTransform(scrollYProgress, [0, 1.01], [50, 0]);
 
   return (
-    <div className='containerTeste'>
+    <div ref={container} className='containerTeste z-20 bg-black relative'>
       <h2 className='text-9xl text-white mb-24 font-black tracking-tighter leading-[0.8]'>
         COURSES
         <br />
@@ -33,7 +39,7 @@ export function Courses() {
 
         <div className='absolute inset-x-0 inset-y-1/2'>
           <motion.div
-            className='text-[15rem] text-[#242424] opacity-60 font-black text-nowrap tracking-tighter leading-[0.2] m-0 -z-10'
+            className='text-[15rem] text-[#1d1e21] opacity-60 font-black text-nowrap tracking-tighter leading-[0.2] m-0 -z-10'
             style={{ translateX: customTransform }}
           >
             <span>We Build Good Sh*t </span>
@@ -44,6 +50,10 @@ export function Courses() {
           </motion.div>
         </div>
       </div>
+
+      <motion.div style={{ height }} className='bg-red-500 relative mt-28'>
+        <div className='h-[1550%] w-[120%] -left-[10%] rounded-b-[50%] bg-black z-10 absolute shadow-2xl' />
+      </motion.div>
     </div>
   );
 }
