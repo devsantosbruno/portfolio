7ca842fafbfed08251630b26e487aedce7a19610
bruno santos <devbrunosantos@gmail.com>
2024-03-19 21:21:12 -0300

    feat: Techs Hero Parallax

---
+++ src/components/Techs.tsx
@@ -0,0 +1,78 @@
+import { HeroParallax } from './ui/hero-parallax';
+
+export function Techs() {
+  const techs = [
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+    {
+      title: 'React',
+      link: 'https://react.dev/',
+      image: 'react.svg',
+    },
+  ];
+
+  return <HeroParallax techs={techs} />;
+}
