5d5a6a585721c395c4d1b61b5ff3ceeb643344b2
bruno santos <devbrunosantos@gmail.com>
2024-03-23 12:03:31 -0300

    feat: page with sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Contact } from '@/components/Contact';
+import { Courses } from '@/components/Courses';
 import { Projects } from '@/components/Projects';
-import { MaskContainer } from '@/components/ui/svg-mask-effect';
+import { Techs } from '@/components/Techs';
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 
 const Scene = dynamic(() => import('@/components/Scene/Index'), {
-  ssr: false,
+  ssr: true,
 });
 
 export default function Home() {
@@ -14,44 +15,61 @@ export default function Home() {
         <Scene />
       </section>
 
-      <section className='bg-white'>
-        <MaskContainer
-          revealText={
-            <h2 className='text-8xl text-lime-400 mb-10 font-black tracking-tighter'>
-              HOVER HERE
-            </h2>
-          }
-        >
-          <h1>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
-            ratione rem veritatis repudiandae ullam dolore minus accusantium
-            alias ut, eos eum suscipit dignissimos, laborum sed perferendis!
-            Aspernatur, veniam totam. Praesentium. The first rule of{' '}
-            <span className='text-lime-400'>MRR Club</span> is you do not talk
-            about MRR Club. The second rule of MRR Club is you DO NOT talk about{' '}
-            <span className='text-lime-400'>MRR Club</span>.
-          </h1>
-        </MaskContainer>
-      </section>
+      <section className='w-screen min-h-screen relative bg-[#242424] flex flex-col justify-between'>
+        <Image
+          alt=''
+          src='/images/about.webp'
+          width={1920}
+          height={1080}
+          className='absolute inset-x-0 bottom-0 object-contain w-full h-full'
+        />
 
-      <section className='py-20'>
-        <div className='container'>
-          <h2 className='text-8xl text-lime-400 mb-24 font-black tracking-tighter'>
-            Contato
+        <div className='containerTeste bg-transparent mb-10'>
+          <h2 className='text-6xl md:text-9xl lg:text-[15rem] text-white mb-24 font-black tracking-tighter leading-[0.8] bg-transparent'>
+            ABOUT <br /> ME
           </h2>
 
-          <Contact />
+          <div className='-mt-80 w-1/2 ml-auto relative z-10 flex flex-col gap-10'>
+            <h3 className='text-6xl text-white font-thin'>
+              Hello, my name's Bruno, I'm a Tech Lead based in Porto Alegre. At
+              Hero99, I serve as a Tech Lead, actively contributing to the
+              development of applications, systems, and websites.
+            </h3>
+            <h3 className='text-6xl text-white font-thin'>
+              When I'm not grinding at work, you can catch me pumping iron at
+              the gym, honing my poker skills or planning my next adventure to
+              explore the world.
+            </h3>
+          </div>
         </div>
       </section>
 
+      <section className='w-screen min-h-screen flex flex-col justify-between'>
+        <Techs />
+      </section>
+
       <section className='bg-white pt-20'>
-        <div className='container'>
-          <h2 className='text-8xl text-black mb-24 font-black tracking-tighter'>
+        <div className='containerTeste'>
+          <h2 className='text-6xl md:text-8xl text-black mb-16 md:mb-24 font-black tracking-tighter'>
             PROJECTS
           </h2>
         </div>
 
         <Projects />
+      </section>
+
+      <section className='py-20 bg-black'>
+        <div className='containerTeste'>
+          <h2 className='text-6xl md:text-9xl text-white mb-16 md:mb-24 font-black tracking-tighter leading-[0.8]'>
+            COURSES
+            <br />
+            AND
+            <br />
+            CERTIFIERS
+          </h2>
+        </div>
+
+        <Courses />
       </section>
     </>
   );
