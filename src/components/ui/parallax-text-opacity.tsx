aca57aa6df14f37e0a28ee6b4fbba23e1e2b5dea
bruno santos <devbrunosantos@gmail.com>
2024-03-25 18:37:00 -0300

    feat: useScroll hook

--- src/components/ui/parallax-text-opacity.tsx
+++ src/components/ui/parallax-text-opacity.tsx
@@ -1,69 +1,65 @@
-"use client";
+'use client';
 
-import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { useScroll } from '@/hooks/useScroll';
+import { motion, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export function Parallax({ paragraph }: any) {
-	const container = useRef(null);
+  const container = useRef(null);
 
-	const { scrollYProgress } = useScroll({
-		target: container,
+  const scrollYProgress = useScroll(container, ['start 0.9', 'start 0.25']);
+  const words = paragraph.split(' ');
 
-		offset: ["start 0.9", "start 0.25"],
-	});
+  return (
+    <p
+      ref={container}
+      className='flex text-[60px] p-10 max-w-[1280px] flex-wrap'
+    >
+      {words.map((word, i) => {
+        const start = i / words.length;
 
-	const words = paragraph.split(" ");
+        const end = start + 1 / words.length;
 
-	return (
-		<p
-			ref={container}
-			className="flex text-[60px] p-10 max-w-[1280px] flex-wrap"
-		>
-			{words.map((word, i) => {
-				const start = i / words.length;
-
-				const end = start + 1 / words.length;
-
-				return (
-					<Word key={i} progress={scrollYProgress} range={[start, end]}>
-						{word}
-					</Word>
-				);
-			})}
-		</p>
-	);
+        return (
+          <Word key={i} progress={scrollYProgress} range={[start, end]}>
+            {word}
+          </Word>
+        );
+      })}
+    </p>
+  );
 }
 
 const Word = ({ children, progress, range }: any) => {
-	const amount = range[1] - range[0];
+  const amount = range[1] - range[0];
 
-	const step = amount / children.length;
+  const step = amount / children.length;
 
-	return (
-		<span className="relative mr-3 mt-3">
-			{children.split("").map((char, i) => {
-				const start = range[0] + i * step;
+  return (
+    <span className='relative mr-3 mt-3'>
+      {children.split('').map((char, i) => {
+        const start = range[0] + i * step;
 
-				const end = range[0] + (i + 1) * step;
+        const end = range[0] + (i + 1) * step;
 
-				return (
-					<Char key={`c_${i}`} progress={progress} range={[start, end]}>
-						{char}
-					</Char>
-				);
-			})}
-		</span>
-	);
+        return (
+          <Char key={`c_${i}`} progress={progress} range={[start, end]}>
+            {char}
+          </Char>
+        );
+      })}
+    </span>
+  );
 };
 
 const Char = ({ children, progress, range }: any) => {
-	const opacity = useTransform(progress, range, [0, 1]);
+  const opacity = useTransform(progress, range, [0, 1]);
 
-	return (
-		<span>
-			<span className="absolute opacity-20">{children}</span>
+  return (
+    <span>
+      <span className='absolute opacity-20'>{children}</span>
 
-			<motion.span style={{ opacity: opacity }}>{children}</motion.span>
-		</span>
-	);
+      <motion.span style={{ opacity: opacity }}>{children}</motion.span>
+    </span>
+  );
 };
