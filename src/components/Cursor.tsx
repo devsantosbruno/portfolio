c53edb52b2e2e3c54a65562670c4fc99e87fe405
bruno santos <devbrunosantos@gmail.com>
2024-03-30 15:07:35 -0300

    feat: Cursor component

---
+++ src/components/Cursor.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { motion, useMotionValue } from 'framer-motion';
+import { useEffect } from 'react';
+
+export function Cursor() {
+  const cursorX = useMotionValue(-100);
+  const cursorY = useMotionValue(-100);
+
+  useEffect(() => {
+    const moveCursor = (e) => {
+      cursorX.set(e.clientX - 16);
+      cursorY.set(e.clientY - 16);
+    };
+
+    window.addEventListener('mousemove', moveCursor);
+
+    return () => {
+      window.removeEventListener('mousemove', moveCursor);
+    };
+  }, [cursorX, cursorY]);
+
+  return (
+    <motion.div
+      className='fixed left-0 top-0 w-8 h-8 hover:w-20 hover:h-20 rounded-2xl mix-blend-difference bg-lime-400 z-[999] pointer-events-none transition duration-500 ease-out'
+      style={{
+        translateX: cursorX,
+        translateY: cursorY,
+      }}
+    />
+  );
+}
