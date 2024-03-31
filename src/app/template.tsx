364e5d31399eafdb641d34586305254d77cc7843
bruno santos <devbrunosantos@gmail.com>
2024-03-31 15:48:06 -0300

    feat: Preloader component

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,10 +1,34 @@
+'use client';
+
+import { Cursor } from '@/components/Cursor';
 import { PageTransition } from '@/components/PageTransition';
+import { Preloader } from '@/components/Preloader';
+import { AnimatePresence } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+      window.scrollTo(0, 0);
+    }, 3500);
+  }, []);
+
   return (
     <div>
-      <PageTransition />
-      {children}
+      <AnimatePresence mode='wait'>
+        {isLoading ? (
+          <Preloader />
+        ) : (
+          <>
+            <Cursor />
+            <PageTransition />
+            {children}
+          </>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
