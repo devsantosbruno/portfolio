aa10333d2601f0067e7ea7c036b86e1832200f9f
bruno santos <devbrunosantos@gmail.com>
2024-04-01 08:59:16 -0300

    style: PageTransition z-index

--- src/components/PageTransition/index.tsx
+++ src/components/PageTransition/index.tsx
@@ -9,7 +9,7 @@ export function PageTransition() {
   }, []);
 
   return (
-    <>
+    <div className='relative z-50'>
       <div
         id='banner-1'
         className='min-h-screen bg-neutral-950 z-10 fixed top-0 left-0 w-1/4'
@@ -26,6 +26,6 @@ export function PageTransition() {
         id='banner-4'
         className='min-h-screen bg-neutral-950 z-10 fixed top-0 left-3/4 w-1/4'
       />
-    </>
+    </div>
   );
 }
