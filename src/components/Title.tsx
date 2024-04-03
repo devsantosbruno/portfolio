48017c00695bd90cee164bf9d1d60c9ff3ab8bc5
bruno santos <devbrunosantos@gmail.com>
2024-04-03 12:40:18 -0300

    feat: Title with ReactNode

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,19 +1,21 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 type TitleProps = {
-	children: React.ReactElement;
-	className?: string;
+  children: React.ReactNode;
+  className?: string;
+  id?: string;
 };
 
-export function Title({ children, className }: TitleProps) {
-	return (
-		<h2
-			className={cn(
-				"text-[3.75rem] md:text-[6rem] font-black tracking-tighter leading-[0.8] text-white mb-16 md:mb-24",
-				className,
-			)}
-		>
-			{children}
-		</h2>
-	);
+export function Title({ children, className, id }: TitleProps) {
+  return (
+    <h2
+      className={cn(
+        'text-[3.75rem] md:text-[6rem] font-black tracking-tighter leading-[0.8] text-white mb-16 md:mb-24',
+        className
+      )}
+      id={id}
+    >
+      {children}
+    </h2>
+  );
 }
