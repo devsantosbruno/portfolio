8ca4ae3dbf217e0564516aab3c71039de00493d9
bruno santos <devbrunosantos@gmail.com>
2024-04-16 08:37:32 -0300

    chore: biome format

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -1,57 +1,57 @@
 export type ProjectType = {
-  slug: string;
-  title: string;
-  creativeTitle: string;
-  year: string;
-  banner: string;
-  mockups: string[];
-  link: string;
-  jobs: string[];
-  resume: string;
-  description: string;
+	slug: string;
+	title: string;
+	creativeTitle: string;
+	year: string;
+	banner: string;
+	mockups: string[];
+	link: string;
+	jobs: string[];
+	resume: string;
+	description: string;
 };
 
 export const projects: ProjectType[] = [
-  {
-    slug: 'silence',
-    title: 'Silence',
-    creativeTitle: 'creative text creative text creative text',
-    year: '2024',
-    banner: 'silencio.png',
-    mockups: ['officestudio.png', 'locomotive.png', 'c2montreal.png'],
-    link: 'https://devbruno.me/',
-    jobs: ['development front'],
-    resume:
-      "'C'est avec honneur que nous avons eu l'opportunité de créer le site vitrine d'Enchanted Tools, une start-up française développant des robots intelligents pour faciliter les opérations dans les milieux hospitaliers. En collaboration avec Antony Legrand pour la partie Direction Artistique.",
-    description:
-      "Pour la landing page d'Enchanted Tools, l'objectif était de mettre en avant le personnage principal : le robot. Nous avons opté pour un rendu 3D très zoomé afin de permettre aux visiteurs de découvrir en détail les caractéristiques du robot tout en faisant défiler la page. Nous avons collaboré avec Hocus Pocus pour la réalisation de la 3D du robot, et de notre côté, nous avons pris en charge le développement WebGL pour permettre au robot de tourner au fur et à mesure du scroll.",
-  },
-  {
-    slug: 'coffee-delivery',
-    title: 'Coffee Delivery',
-    creativeTitle: 'coffee delivery with creative text',
-    year: '2023',
-    banner: 'c2montreal.png',
-    mockups: ['locomotive.png', 'silencio.png', 'officestudio.png'],
-    link: 'https://devbruno.me/',
-    jobs: ['development front'],
-    resume:
-      "'C'est avec honneur que nous avons eu l'opportunité de créer le site vitrine d'Enchanted Tools, une start-up française développant des robots intelligents pour faciliter les opérations dans les milieux hospitaliers. En collaboration avec Antony Legrand pour la partie Direction Artistique.",
-    description:
-      "Pour la landing page d'Enchanted Tools, l'objectif était de mettre en avant le personnage principal : le robot. Nous avons opté pour un rendu 3D très zoomé afin de permettre aux visiteurs de découvrir en détail les caractéristiques du robot tout en faisant défiler la page. Nous avons collaboré avec Hocus Pocus pour la réalisation de la 3D du robot, et de notre côté, nous avons pris en charge le développement WebGL pour permettre au robot de tourner au fur et à mesure du scroll.",
-  },
-  {
-    slug: 'bruno-shop',
-    title: 'Bruno Shop',
-    creativeTitle: 'creative text with Bruno Shop',
-    year: '2023',
-    banner: 'officestudio.png',
-    mockups: ['c2montreal.png', 'silencio.png', 'locomotive.png'],
-    link: 'https://devbruno.me/',
-    jobs: ['development front'],
-    resume:
-      "'C'est avec honneur que nous avons eu l'opportunité de créer le site vitrine d'Enchanted Tools, une start-up française développant des robots intelligents pour faciliter les opérations dans les milieux hospitaliers. En collaboration avec Antony Legrand pour la partie Direction Artistique.",
-    description:
-      "Pour la landing page d'Enchanted Tools, l'objectif était de mettre en avant le personnage principal : le robot. Nous avons opté pour un rendu 3D très zoomé afin de permettre aux visiteurs de découvrir en détail les caractéristiques du robot tout en faisant défiler la page. Nous avons collaboré avec Hocus Pocus pour la réalisation de la 3D du robot, et de notre côté, nous avons pris en charge le développement WebGL pour permettre au robot de tourner au fur et à mesure du scroll.",
-  },
+	{
+		slug: "silence",
+		title: "Silence",
+		creativeTitle: "creative text creative text creative text",
+		year: "2024",
+		banner: "silencio.png",
+		mockups: ["officestudio.png", "locomotive.png", "c2montreal.png"],
+		link: "https://devbruno.me/",
+		jobs: ["development front"],
+		resume:
+			"'C'est avec honneur que nous avons eu l'opportunité de créer le site vitrine d'Enchanted Tools, une start-up française développant des robots intelligents pour faciliter les opérations dans les milieux hospitaliers. En collaboration avec Antony Legrand pour la partie Direction Artistique.",
+		description:
+			"Pour la landing page d'Enchanted Tools, l'objectif était de mettre en avant le personnage principal : le robot. Nous avons opté pour un rendu 3D très zoomé afin de permettre aux visiteurs de découvrir en détail les caractéristiques du robot tout en faisant défiler la page. Nous avons collaboré avec Hocus Pocus pour la réalisation de la 3D du robot, et de notre côté, nous avons pris en charge le développement WebGL pour permettre au robot de tourner au fur et à mesure du scroll.",
+	},
+	{
+		slug: "coffee-delivery",
+		title: "Coffee Delivery",
+		creativeTitle: "coffee delivery with creative text",
+		year: "2023",
+		banner: "c2montreal.png",
+		mockups: ["locomotive.png", "silencio.png", "officestudio.png"],
+		link: "https://devbruno.me/",
+		jobs: ["development front"],
+		resume:
+			"'C'est avec honneur que nous avons eu l'opportunité de créer le site vitrine d'Enchanted Tools, une start-up française développant des robots intelligents pour faciliter les opérations dans les milieux hospitaliers. En collaboration avec Antony Legrand pour la partie Direction Artistique.",
+		description:
+			"Pour la landing page d'Enchanted Tools, l'objectif était de mettre en avant le personnage principal : le robot. Nous avons opté pour un rendu 3D très zoomé afin de permettre aux visiteurs de découvrir en détail les caractéristiques du robot tout en faisant défiler la page. Nous avons collaboré avec Hocus Pocus pour la réalisation de la 3D du robot, et de notre côté, nous avons pris en charge le développement WebGL pour permettre au robot de tourner au fur et à mesure du scroll.",
+	},
+	{
+		slug: "bruno-shop",
+		title: "Bruno Shop",
+		creativeTitle: "creative text with Bruno Shop",
+		year: "2023",
+		banner: "officestudio.png",
+		mockups: ["c2montreal.png", "silencio.png", "locomotive.png"],
+		link: "https://devbruno.me/",
+		jobs: ["development front"],
+		resume:
+			"'C'est avec honneur que nous avons eu l'opportunité de créer le site vitrine d'Enchanted Tools, une start-up française développant des robots intelligents pour faciliter les opérations dans les milieux hospitaliers. En collaboration avec Antony Legrand pour la partie Direction Artistique.",
+		description:
+			"Pour la landing page d'Enchanted Tools, l'objectif était de mettre en avant le personnage principal : le robot. Nous avons opté pour un rendu 3D très zoomé afin de permettre aux visiteurs de découvrir en détail les caractéristiques du robot tout en faisant défiler la page. Nous avons collaboré avec Hocus Pocus pour la réalisation de la 3D du robot, et de notre côté, nous avons pris en charge le développement WebGL pour permettre au robot de tourner au fur et à mesure du scroll.",
+	},
 ];
