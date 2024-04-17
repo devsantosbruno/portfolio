ed7a78599fbaafc6c85257b867f4be7f868a2300
bruno santos <devbrunosantos@gmail.com>
2024-04-16 21:43:17 -0300

    feat: show Cursor if not mobile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -59,7 +59,10 @@ export default function Contact() {
 					</h2>
 
 					<div className="mt-5 flex justify-end">
-						<button className="inline-flex animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#a3e635,55%,#000103)] bg-[length:200%_100%] disabled:bg-none disabled:opacity-50 px-10 py-3 font-medium text-slate-400 transition-colors">
+						<button
+							type="button"
+							className="inline-flex animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#a3e635,55%,#000103)] bg-[length:200%_100%] disabled:bg-none disabled:opacity-50 px-10 py-3 font-medium text-slate-400 transition-colors"
+						>
 							submit
 						</button>
 					</div>
