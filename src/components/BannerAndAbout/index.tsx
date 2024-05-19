04eecf7f4a6a041f7b88d1e902d091224df6bad3
bruno santos <devbrunosantos@gmail.com>
2024-05-19 15:53:04 -0300

    feat: Banner and About with animation

--- src/components/BannerAndAbout/index.tsx
+++ src/components/BannerAndAbout/index.tsx
@@ -80,6 +80,7 @@ export function BannerAndAbout() {
 				<Container className="relative mr-auto">
 					<h2 className="text-[3.75rem] md:text-[6rem] font-black tracking-tighter leading-[0.8] text-white mb-0">
 						<span>Hello </span>
+
 						<motion.span
 							id="initialElement"
 							style={{
@@ -98,39 +99,36 @@ export function BannerAndAbout() {
 			</section>
 
 			<section className="w-screen h-screen relative bg-[#242424] flex flex-col justify-between py-10">
-				<Image
-					alt=""
-					src="/images/about.webp"
-					width={1920}
-					height={1080}
-					className="absolute inset-x-0 bottom-0 object-contain w-full h-full"
-				/>
+				<div className="absolute inset-x-0 bottom-0 lg:w-[60vw] xl:w-[40vw] mx-auto h-screen overflow-visible z-20">
+					<Image
+						alt=""
+						src="/images/about.webp"
+						width={1920}
+						height={1080}
+						className="object-cover w-full h-full overflow-visible relative z-20"
+					/>
+				</div>
 
-				<Container className="bg-transparent mb-10 grid grid-cols-2 gap-10">
-					<h2 className="text-6xl md:text-9xl lg:text-[15rem] text-white mb-24 font-black tracking-tighter leading-[0.8] bg-transparent">
-						ABOUT <br /> ME
-					</h2>
+				<Container className="bg-transparent mb-10 grid grid-cols-2">
+					<div />
 
-					<h2 className="text-[3.75rem] md:text-[6rem] font-black tracking-tighter leading-[0.8] text-white mb-0 text-end">
-						SOFT
-						<span className="opacity-0" id="endElement">
-							W
-						</span>
-						ARE DEVELOPER
-					</h2>
+					<div className="">
+						<h2 className="text-[3.75rem] md:text-[6rem] font-black tracking-tighter leading-[0.8] text-white mb-0 text-end">
+							I AM A SOFT
+							<span className="opacity-0" id="endElement">
+								W
+							</span>
+							ARE DEVELOPER
+						</h2>
 
-					{/* <div className='-mt-80 w-1/2 ml-auto relative z-10 flex flex-col gap-10'>
-            <h3 className='text-6xl text-white font-thin'>
-              Hello, my name's Bruno, I'm a Tech Lead based in Porto Alegre. At
-              Hero99, I serve as a Tech Lead, actively contributing to the
-              development of applications, systems, and websites.
-            </h3>
-            <h3 className='text-6xl text-white font-thin'>
-              When I'm not grinding at work, you can catch me pumping iron at
-              the gym, honing my poker skills or planning my next adventure to
-              explore the world.
-            </h3>
-          </div> */}
+						<div className="relative z-20 w-3/4 ml-auto">
+							<h3 className="text-[3rem] text-white font-thin tracking-tighter leading-[0.8] mt-8">
+								My vibe is to explore animations, with GSAP, Framer Motion, or
+								Three.js. I pass the interface, adding special “seasoning,” of
+								life and personality to the interfaces.
+							</h3>
+						</div>
+					</div>
 				</Container>
 			</section>
 		</>
