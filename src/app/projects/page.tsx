04552d16702ef78b69c3bab599955118041c3462
bruno santos <devbrunosantos@gmail.com>
2024-05-20 00:32:07 -0300

    feat: Projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,69 +1,60 @@
 "use client";
 
-import { Container, Input, Title } from "@/components";
+import { Container } from "@/components";
 import { useScroll } from "@/hooks/useScroll";
+import { projects } from "@/mocks/projects";
 import { motion, useTransform } from "framer-motion";
 import { useRef } from "react";
+import { ProjectSummary } from "./components/ProjectSummary";
 
-export default function Contact() {
+const firstColumn = projects.slice(0, Math.ceil(projects.length / 2));
+const secondColumn = projects.slice(
+	Math.ceil(projects.length / 2),
+	projects.length,
+);
+
+export default function Projects() {
 	const container = useRef(null);
-	const scrollYProgress = useScroll(container, ["start end", "end start"]);
-	const height = useTransform(scrollYProgress, [0, 1.01], [50, 0]);
+	const scrollPage = useScroll(container, ["start end", "end start"]);
+	const height = useTransform(scrollPage, [0, 1.01], [50, 0]);
+	const background = useTransform(
+		scrollPage,
+		[0.16, 0.33, 0.66],
+		["#ffffff", "#a3e635", "#000000"],
+	);
 
 	return (
-		<section
+		<motion.section
 			ref={container}
-			className="z-10 relative bg-black py-20"
+			className="z-10 relative pt-20"
+			style={{ background }}
 			id="contact"
 		>
 			<Container className="min-h-[125vh]">
-				<Title className="uppercase mr-auto pt-[15vh]">
-					Preencha os campos e <br /> entraremos em contato
-				</Title>
+				<h1 className="text-[3.75rem] md:text-[8rem] bg-clip-text text-transparent bg-gradient-to-r from-black to-40% to-lime-400 mb-16 md:mb-24 font-black tracking-tighter leading-[0.8]">
+					PROJECTS
+					<br />
+					DEVELOPED
+					<br />
+					BY ME
+				</h1>
 
-				<div className="flex flex-col gap-10">
-					<h2 className="text-4xl text-white">
-						Meu nome é{" "}
-						<span className="whitespace-nowrap">
-							[
-							<Input />
-							],
-						</span>{" "}
-						Eu sou{" "}
-						<span className="whitespace-nowrap">
-							[
-							<Input />
-							],
-						</span>{" "}
-						Falo em nome da empresa{" "}
-						<span className="whitespace-nowrap">
-							[
-							<Input />
-							],
-						</span>{" "}
-						Gostaria de marcar uma reunião para entender melhor o trabalho de
-						vocês e se minha marca é compatível com o serviço de vocês.
-					</h2>
+				<div className="grid grid-cols-2 gap-10">
+					<div className="flex flex-col gap-40">
+						{firstColumn.map((item, index) => (
+							<ProjectSummary key={item.slug} index={index} project={item} />
+						))}
+					</div>
 
-					<h2 className="text-4xl text-white">
-						Quer um orçamento? Vamos marcar uma reunião para que possamos
-						entender melhor sua empresa e como podemos fazer dela melhor,
-						juntos. Qual seu email?{" "}
-						<span className="whitespace-nowrap">
-							[
-							<Input />
-							],
-						</span>{" "}
-						Entraremos em contato para um agendamento, muito obrigado.
-					</h2>
-
-					<div className="mt-5 flex justify-end">
-						<button
-							type="button"
-							className="inline-flex animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#a3e635,55%,#000103)] bg-[length:200%_100%] disabled:bg-none disabled:opacity-50 px-10 py-3 font-medium text-slate-400 transition-colors"
-						>
-							submit
-						</button>
+					<div className="flex flex-col gap-20">
+						{secondColumn.map((item, index) => (
+							<ProjectSummary
+								key={item.slug}
+								index={index}
+								project={item}
+								negative
+							/>
+						))}
 					</div>
 				</div>
 			</Container>
@@ -71,6 +62,6 @@ export default function Contact() {
 			<motion.div style={{ height }} className="relative mt-28">
 				<div className="h-[1550%] w-[120%] -left-[10%] rounded-b-[50%] bg-black z-10 absolute shadow-contact" />
 			</motion.div>
-		</section>
+		</motion.section>
 	);
 }
