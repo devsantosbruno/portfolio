fbf70e71ddf1a82b3381387eb3753b9baeda2243
bruno santos <devbrunosantos@gmail.com>
2024-05-21 21:46:29 -0300

    feat: courses and certifiers

--- src/components/Courses/Card.tsx
+++ src/components/Courses/Card.tsx
@@ -1,23 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export type CardProps = {
-	company: string;
+	title: string;
 	image: string;
-	link: string;
 	logo: string;
 	techs: string[];
-	year: string;
+	link: string;
 };
 
-export function Card({ company, image, link, logo, techs, year }: CardProps) {
+export function Card({ title, image, link, logo, techs }: CardProps) {
 	return (
 		<div className="text-white w-full md:max-w-[35%] overflow-hidden z-10 px-10">
-			<div className="bg-lime-500 rounded-3xl h-[600px] flex relative overflow-hidden">
-				<div className="absolute top-5 right-5 bg-lime-700/50 rounded-full h-28 w-28 flex items-center justify-center">
-					<Image src={`/${logo}`} width={80} height={80} alt="" />
+			<div className="border border-lime-500 rounded-3xl h-[600px] flex relative overflow-hidden">
+				<div className="absolute inset-0 rounded-3xl">
+					<Image
+						src={image}
+						width={1920}
+						height={1080}
+						alt=""
+						className="h-full w-full bg-cover rounded-3xl"
+					/>
+					<Link href={link} target="_blank" className="absolute inset-0" />
 				</div>
 
-				<h2>IMAGEM</h2>
+				<div className="absolute top-5 right-5 bg-lime-700/75 rounded-full h-28 w-28 flex items-center justify-center">
+					<Image src={`/${logo}`} width={80} height={80} alt="" />
+				</div>
 			</div>
 
 			<div className="flex flex-col mt-12">
@@ -26,13 +35,25 @@ export function Card({ company, image, link, logo, techs, year }: CardProps) {
 				</span>
 
 				<h3 className="text-4xl text-[#FEFEFE] font-semibold mt-2 mb-8">
-					{company}
+					{title}
 				</h3>
 
 				<div className="flex gap-14 text-[#4B4E4B]">
-					<span>Link</span>
+					<Link
+						href={link}
+						target="_blank"
+						className="hover:text-lime-300 transition duration-300"
+					>
+						Link
+					</Link>
 					<span>*</span>
-					<span>Link</span>
+					<Link
+						href={link}
+						target="_blank"
+						className="hover:text-lime-300 transition duration-300"
+					>
+						Link
+					</Link>
 				</div>
 			</div>
 		</div>
