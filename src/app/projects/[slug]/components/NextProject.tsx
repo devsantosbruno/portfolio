38c185458df3aa910715e07328d6bdbed71a8204
bruno santos <devbrunosantos@gmail.com>
2024-06-05 18:32:44 -0300

    fix: bugs and responsive

--- src/app/projects/[slug]/components/NextProject.tsx
+++ src/app/projects/[slug]/components/NextProject.tsx
@@ -30,7 +30,7 @@ export function NextProject({ project, projectsQuantity }: NextProject) {
 	return (
 		<div className="bg-white text-center text-black">
 			<Container>
-				<div className="pt-80 pb-52 flex justify-center text-center items-center">
+				<div className="py-40 lg:pt-80 lg:pb-52 flex justify-center text-center items-center">
 					<h2
 						className="uppercase text-black font-thin text-center tracking-widest leading-[0.8] px-4 py-4"
 						style={{ fontSize: `${(enjoyingSize * 1.5) / 16}rem` }}
@@ -41,12 +41,12 @@ export function NextProject({ project, projectsQuantity }: NextProject) {
 
 				<div className="flex justify-center items-center border-b border-black">
 					<div className="relative overflow-hidden group">
-						<div className="-mb-8">
+						<div className="mb-8 lg:-mb-8">
 							<span className="text-2xl font-thin tracking-tighter leading-[0.8]">
 								Next Project
 							</span>
-							<div className="group-hover:scale-75 transition duration-500 mt-5">
-								<h2 className="text-8xl font-bold tracking-tighter leading-[0.8] text-black group-hover:text-slate-800 transition duration-1000">
+							<div className="lg:group-hover:scale-75 transition duration-500 mt-5">
+								<h2 className="text-8xl font-bold tracking-tighter leading-[0.8] text-black lg:group-hover:text-slate-800 transition duration-1000">
 									{title}
 								</h2>
 							</div>
@@ -56,7 +56,7 @@ export function NextProject({ project, projectsQuantity }: NextProject) {
 							src={`/images/${banner}`}
 							width={800}
 							height={800}
-							className="w-full max-w-[600px] h-[400px] object-cover object-center translate-y-[300px] group-hover:translate-y-0 transition duration-500 shadow-2xl shadow-black"
+							className="w-full max-w-[600px] h-[400px] object-cover object-center lg:translate-y-[300px] lg:group-hover:translate-y-0 transition duration-500 shadow-2xl shadow-black"
 							alt={`${title} preview`}
 						/>
 
@@ -70,7 +70,7 @@ export function NextProject({ project, projectsQuantity }: NextProject) {
 
 				<button
 					type="button"
-					className="mx-auto mt-20 w-fit flex items-center justify-center rounded-full border px-10 py-3 font-thin bg-[#242424] hover:bg-lime-400 border-white hover:border-lime-400 text-white hover:text-[#242424] shadow-2xl hover:shadow-lime-400 transition duration-500"
+					className="mx-auto mt-20 w-fit flex items-center justify-center rounded-full border px-10 py-3 font-thin bg-[#242424] lg:hover:bg-lime-400 border-white lg:hover:border-lime-400 text-white lg:hover:text-[#242424] shadow-2xl lg:hover:shadow-lime-400 transition duration-500"
 					onClick={() => animatePageIn("/projects", router)}
 				>
 					all projects<sup>{projectsQuantity}</sup>
